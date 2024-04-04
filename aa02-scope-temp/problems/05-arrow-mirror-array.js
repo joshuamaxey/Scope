1336098d21cd6f208ca6073c49f6ea1b0f44db38
Khalid <mugen89k@gmail.com>
2024-04-04 18:31:30 -0400

    05 done

--- aa02-scope-temp/problems/05-arrow-mirror-array.js
+++ aa02-scope-temp/problems/05-arrow-mirror-array.js
@@ -10,7 +10,7 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here 
+const arrowMirrorArray = array => [...array, ...array.slice().reverse()]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
