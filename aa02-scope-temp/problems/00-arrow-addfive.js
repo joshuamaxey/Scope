ac18e44f236b485bad6500b8ce50ae4aca7f75e1
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-03 19:10:11 -0500

    Ran npm install and finished problem 1

--- aa02-scope-temp/problems/00-arrow-addfive.js
+++ aa02-scope-temp/problems/00-arrow-addfive.js
@@ -15,7 +15,21 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+let addFive = num => {return num + 5};
+
+// For comparison, THIS is what the function would look like with regular syntax:
+
+// function addFiveLong(num) {
+//   return num + 5;
+// }
+
+let result1 = addFive(0); // returns 5
+let result2 = addFive(10); // returns 15
+let result3 = addFive(37); // returns 42
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
